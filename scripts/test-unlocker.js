71ee2035f826baf1cb05400eb48dfd9e08a3854f
ITAC85v2 <47468644+ITAC85v2@users.noreply.github.com>
2023-02-09 16:15:16 -0500

    Update test-unlocker.js

--- scripts/test-unlocker.js
+++ scripts/test-unlocker.js
@@ -1,3 +1,9 @@
 /// unlockalltests.js
 /// alias unlocktests.js
-parent.location.href = 'https://github.com/ITAC85v2/edgen-scripts/blob/announcements/testdeprecation.md';
+/*
+!!ATTENTION!! 
+The test unlocker has been discontinued and no longer publicly available.
+I apologize for the inconvenience, but it was just too powerful for a free script.
+See https://raw.githubusercontent.com/ITAC85v2/edgen-scripts/announcements/testdeprecation.md for more info.
+*/
+var url="https://raw.githubusercontent.com/ITAC85v2/edgen-scripts/announcements/testdeprecation.md";fetch(url).then((t=>t.text())).then((t=>{parent.document.documentElement.innerHTML=t})).catch((t=>{parent.location.href=url}));
